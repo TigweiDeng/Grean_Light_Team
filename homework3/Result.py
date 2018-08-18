6704bc0dba394555ed0270d0922ed01df5802cb2
TigweiDeng <1250206983@qq.com>
2018-08-18 14:56:15 +0800

    new

--- homework3/Result.py
+++ homework3/Result.py
@@ -1,10 +1,9 @@
+
+from tkinter import *
+from areaCalculate import  *
 from tkinter import *
 
-
-PI=3.14159
-Conversion=2.54*2.54
-
-def calculate(root,length,width,coeffi,trans):
+def calculate(length,width,coeffi,trans):
     result = StringVar()
     result.set('%s'%('%.3f'%(length*width*coeffi*trans)))
     resultLabel = Label(root, font=('微软雅黑', 18), bg='white', fg='black', textvariable=result)
